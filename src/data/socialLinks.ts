40b37f8b34932decd89bf9dda3e1cde79c5f7aff
Pintu kumar <pintu1012kumar@gmail.com>
2025-04-08 01:54:51 +0530

    update projects and social media

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -1,26 +1,20 @@
 export const socialLinks = [
   {
     title: "GitHub",
-    url: "https://github.com/amaan8429",
+    url: "",
   },
   {
     title: "LinkedIn",
-    url: "https://www.linkedin.com/in/amaan8429",
+    url: "",
   },
   {
     title: "Twitter",
-    url: "https://twitter.com/amaan8429",
+    url: "",
   },
-  {
-    title: "Instagram",
-    url: "https://www.instagram.com/amaan.8429",
-  },
-  {
-    title: "Discord",
-    url: "https://discord.com/users/amaan8429",
-  },
+  
+
   {
     title: "Email",
-    url: "mailto:amaanrizvi73@gmail.com",
+    url: "",
   },
 ];
