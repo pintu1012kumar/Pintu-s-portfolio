35eeb88bac8aeae9fae5be7cf628d3dd1d281a06
Pintu kumar <pintu1012kumar@gmail.com>
2025-04-13 16:40:59 +0530

    add project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -10,5 +10,42 @@ export const projectsData = [
     github: "https://github.com/pintu1012kumar/Blogging-app.git",
     demo: "https://blogging-app-theta-eight.vercel.app/",
   },
+  {
+    title: "100xFaster",
+    description: "This is a user-friendly web application that allows users to book doctor appointments directly from home with just a one click.",
+    techStack: ["Next.js", "Typescript", "Postgres", "Prisma", "Aceternity UI"],
+    features: [
+      "Book your appointments from home ",
+      "Book in advance without waiting in line.",
+      "Save time and crowd",
+      
+    ],
+    github: "https://github.com/pintu1012kumar/100xfaster",
+    demo: "https://100xfaster.vercel.app/",
+  },
+
+  {
+    title: "100xTyper",
+    description: "This is a typing speed test app inspired by MonkeyType to help users improve their typing speed and accuracy.",
+    techStack: ["React.js", "Javascript", "Redux-toolkit", "Tailwind CSS", "Fortawesome"],
+    features: [
+      "Typing speed test",
+      
+    ],
+    github: "https://github.com/pintu1012kumar/100xtyper",
+    demo: "https://100xtyper.vercel.app/",
+  },
+
+  {
+    title: "Paytm-app",
+    description: "This is a Paytm-like application that allows users to add money to their wallet, send money to P2P,and view transaction history.",
+    techStack: ["Next.js", "Express.js" , "Turborepo", "Postgres", "Prisma ORM", "Tailwind", "Docker", "CI-CD", "AWS"],
+    features: [
+      "coming soon",
+      
+    ],
+    github: "https://github.com/pintu1012kumar/paytm-app",
+    demo: "",
+  },
   
 ];
