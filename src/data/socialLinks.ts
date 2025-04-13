06b61aa9e02a42c7c1e1c1c13da3ad4bdbba5c61
Pintu kumar <pintu1012kumar@gmail.com>
2025-04-13 16:47:50 +0530

    add socials

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -1,20 +1,18 @@
 export const socialLinks = [
   {
     title: "GitHub",
-    url: "",
+    url: "https://github.com/pintu1012kumar",
   },
   {
     title: "LinkedIn",
-    url: "",
+    url: "https://www.linkedin.com/in/pintu-kumar1012/",
   },
   {
     title: "Twitter",
-    url: "",
+    url: "https://x.com/Pintu1012kumar",
   },
-  
-
   {
     title: "Email",
-    url: "",
+    url: "Pintu1012kumar@gmail.com",
   },
 ];
